52b1693afd634eea325d1cf92abdd2a24ce0e05a
noka <noka.hieunt@gmail.com>
2022-09-12 20:26:29 +0700

    update

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+/**
+ * @created : ${DATE}, ${DAY_NAME_FULL}
+ **/
+public class Main {
+
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+  }
+}
